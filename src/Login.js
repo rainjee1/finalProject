5e3277c959edf57af52a1e152f386512017893ff
Mehmet Kubilay Kök <kubilaykok1@gmail.com>
2023-05-18 19:02:32 +0300

    update

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+ 
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
+        <div className='bg-white p-3 rounded w-25'>
+            <form action="">
+                <div className='mb-3'>
+                    <label htmlFor="Öğr No.">Öğr No</label>
+                    <input type="integer" placeholder='Öğrenci Numarası'/>
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="password">Şifre</label>
+                    <input type="password" placeholder='Şifre'/>
+                </div>
+                <button className='btn btn-succes w-100 rounded-0'>Giriş Yap</button>
+                <p></p>
+                <a href='' target='blank'  >Şifre Sıfırla</a>
+              
+            </form>
+        </div>
+
+    </div>
   )
 }
 

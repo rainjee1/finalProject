32b284143def5f727d4c5bdd85cfd96ddba5cbf9
Mehmet Kubilay Kök <kubilaykok1@gmail.com>
2023-05-31 20:01:49 +0300

    Firebase Auth.

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4z0HAkXFF0tC2KOgdn0p__67Q12ic2Ns",
+  authDomain: "reactauth-a60ec.firebaseapp.com",
+  projectId: "reactauth-a60ec",
+  storageBucket: "reactauth-a60ec.appspot.com",
+  messagingSenderId: "427535529258",
+  appId: "1:427535529258:web:04efa89e24613c97f41a53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

9225400390ea523e0154f4c51e345156562300dd
Mehmet Kubilay Kök <kubilaykok1@gmail.com>
2023-06-25 19:55:49 +0300

    NavBar

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import SideNav, {Toggle, NavItem, NavIcon, NavText} from '@trendmicro/react-sidenav'
+
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell, faBook, faHome, faNoteSticky, faUserGear } from '@fortawesome/free-solid-svg-icons';
 
 import "@trendmicro/react-sidenav/dist/react-sidenav.css";
+import Navbar from '../Navbar';
 
 function HomePage()
 {
-
   const navigate= useNavigate();
- return  <SideNav
+ return  ( <SideNav
    onSelect={selected => {
        console.log(selected)
        navigate('/'+selected)
    }} 
     
-   >  
+   >,   
+   
      <SideNav.Toggle />
       <SideNav.Nav defaultSelected="HomePage">
       <NavItem eventKey="HomePage">
@@ -48,6 +50,9 @@ function HomePage()
     </SideNav.Nav>
 
     </SideNav>
+  
+ )
+
 } 
 
 export default HomePage;

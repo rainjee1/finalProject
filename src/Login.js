8cf24180d8d3fb3b3d0dd75abfc8c1bf1b2b8832
Mehmet Kubilay Kök <kubilaykok1@gmail.com>
2023-07-11 06:48:02 +0300

    MongoDB

--- src/Login.js
+++ src/Login.js
@@ -1,25 +1,24 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { auth } from "./firebase";
-import { signInWithEmailAndPassword } from "firebase/auth"
-import axios from 'axios';
 
 
-function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const LoginForm = ({ onLogin }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const handleLogin = async (e) => {
-      e.preventDefault();
-      
-      try {
-        const response = await axios.post('mongodb://127.0.0.1:27017/okul', { email, password });
-        console.log(response.data); // Giriş başarılıysa geri dönen verileri işleyebilirsiniz
-      } catch (error) {
-        console.error('Giriş hatası:', error);
-      }
-    }; 
+     fetch(`${'mongodb+srv://new:014500@okull.b21fmkq.mongodb.net/'}/ogr`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({
+        ad, soyad, tags: tags.split(","), body
+      })
+    }).then(resp => resp.json());
+  };
 
   const navigate = useNavigate();
 
@@ -34,18 +33,18 @@ function Login() {
     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
       <div className='bg-white p-3 rounded w-25'>
         <h2>Yaz Okulu Öğrenci Bilgilendirme Sistemi</h2>
-        <form onSubmit={handleLogin}>
+        <form onSubmit={handleSubmit}>
           <div className='mb-3'>
             <label htmlFor="email"><strong>Öğrenci Mail</strong></label>
-            <input type="email"  value={email} pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" onChange={e => setEmail(e.target.value)} placeholder='Öğrenci Mail' className='form-control rounded-0' name='email' />
+            <input type="email"  value={email} pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" onChange={(e) => setEmail(e.target.value)} placeholder='Öğrenci Mail' className='form-control rounded-0' name='email' />
           
           </div>
           <div className='mb-3'>
             <label htmlFor="sifre"><strong>Şifre</strong></label>
-            <input type="password" class="form-control" autocomplete="current-password" placeholder='Şifre' value={password} onChange={e => setPassword(e.target.value)} className='form-control rounded-0' name='sifre' />
+            <input type="password" class="form-control" autocomplete="current-password" placeholder='Şifre' onChange={e => setPassword(e.target.value)} className='form-control rounded-0' name='sifre' />
       
           </div>
-          <button onClick={handleLogin}  className='btn btn-success w-100 rounded-100'><strong>Giriş Yap</strong></button>
+          <button onClick={handleSubmit}  className='btn btn-success w-100 rounded-100'><strong>Giriş Yap</strong></button>
           <p></p>
           <button onClick={navigateToSifreSifirla} className='btn btn-default border w-100 bg-light' target='blank'>Şifre Sıfırla</button>
         </form>
@@ -54,7 +53,7 @@ function Login() {
   );
 }
 
-export default Login;
+export default LoginForm;
 
 
 
